e4db1de8db0b1488ef3c60c2b84de773b3ef06ce
Kevin Kragenbrink <kevin@writh.net>
2024-05-17 02:52:29 -0700

    build: initial commit

---
+++ src/main/java/org/sosly/vlp/VillagerLiteracyProgram.java
@@ -0,0 +1,23 @@
+package org.sosly.vlp;
+
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(VillagerLiteracyProgram.MOD_ID)
+public class VillagerLiteracyProgram {
+    public static final String MOD_ID = "vlp";
+    public static final Logger LOGGER = LogManager.getLogger(VillagerLiteracyProgram.class);
+
+    public VillagerLiteracyProgram() {
+        IEventBus modbus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, org.sosly.vlp.Config.SPEC);
+    }
+}
